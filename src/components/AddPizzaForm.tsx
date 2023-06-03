afae8a3ea2874215b9a42702737ff6bb2e8a9629
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-03 21:02:02 +0300

    add handleSubmit

--- src/components/AddPizzaForm.tsx
+++ src/components/AddPizzaForm.tsx
@@ -15,33 +15,35 @@ const AddPizzaForm: FC = () => {
     const [newPizza, setNewPizza] = useState<{title: string, price: string, img: string }>(initState)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> ) => {
-        console.log('handle change >> ', e.target)
+        const {name, value} = e.target;
+        setNewPizza({...newPizza, [name]: value});
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) =>{
         console.log('handle change >> ', e.target)
+        e.preventDefault();
     }
 
   return (
 
     <form onSubmit={handleSubmit}>
         <input
-         type="title"
-         name="text"
+         type="text"
+         name="title"
          placeholder='Name'
          onChange={handleChange}
          value={newPizza.title} />
 
         <input
-         type="price"
-         name="text"
+         type="text"
+         name="price"
          placeholder='Price'
          onChange={handleChange}
          value={newPizza.price} />
 
         <input
-         type="img"
-         name="text"
+         type="text"
+         name="img"
          placeholder='Image'
          onChange={handleChange}
          value={newPizza.img} />
