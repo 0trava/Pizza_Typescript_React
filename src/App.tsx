b6a6204f50a4fda25e3cf9a3986c2911ef54888a
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-03 21:05:02 +0300

    add component Pizza

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import AddPizzaForm from './components/AddPizzaForm';
+import Pizza from './components/models/Pizza';
 import './App.css';
 
 
+
+
 const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+
+
+
   return (
     <div className="App">
       <div className='wrap'>
